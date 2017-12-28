bedfd2a9119c66f5713e57aa44b087cd0dc9f485
shahabBaloch <30845728+shahabBaloch@users.noreply.github.com>
2017-12-28 11:18:38 -0800

    wct_ Assignment

---
+++ WcfAssignment/WcfAssignment/AptechService1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfAssignment
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AptechService1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AptechService1.svc or AptechService1.svc.cs at the Solution Explorer and start debugging.
+    public class AptechService1 : IAptechService1
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ WcfAssignment/WcfAssignment/IAptechService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfAssignment
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAptechService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAptechService1
+    {
+        [OperationContract]
+        List<Batch> GetAllBatches();
+
+        [OperationContract]
+        List<Student> GetStudntsByBatch(string batch);
+
+        [OperationContract]
+        string MarkAttandance(string sid);
+    }
+}
